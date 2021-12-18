0db5e37bd36d04eb55ce7301538e52c31ebd4383
Hajime Hayano <hajimehayano@gmail.com>
2021-12-17 18:16:17 -0800

    Ensure each character type is included
    [Problem]
    Previous version didn't guarantee the inclusion of all the character
    types. For example, it was possible for the script to generate a
    password without a digit character.
    
    [Solution]
    Always include one character of each character type in all generated
    passwords.

--- password_generator.py
+++ password_generator.py
@@ -4,13 +4,25 @@ import random
 import string
 
 character_pool = ""
-character_pool += string.ascii_letters
+character_pool += string.ascii_lowercase
+character_pool += string.ascii_uppercase
 character_pool += string.digits
 character_pool += string.punctuation
 # character_pool += "_&-#@"
-password_length = 12
-# password_length = 16
+password_length = 16
 
-password = "".join(random.choice(character_pool) for x in range(password_length))
+lowercase_letter = random.choice(string.ascii_lowercase)
+uppercase_letter = random.choice(string.ascii_uppercase)
+digit = random.choice(string.digits) 
+punctuation = random.choice(string.punctuation)
 
-print(password)
+password_requirements = lowercase_letter + uppercase_letter + digit + punctuation
+random_string = "".join(random.choice(character_pool) for x in range(password_length - len(password_requirements)))
+password = password_requirements + random_string
+
+# shuffle the requirement characters to a random part of the string
+list_of_password_characters = list(password)
+random.shuffle(list_of_password_characters)
+shuffled_password = "".join(list_of_password_characters)
+
+print(shuffled_password)
